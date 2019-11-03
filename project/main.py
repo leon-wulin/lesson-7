81f1b9ae53c7dc7b2c7693bf751475365edeceed
leon-wulin <55013445+leon-wulin@users.noreply.github.com>
2019-11-03 10:29:48 +0800

    updated

---
+++ project/main.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+
+from pick_name.naming_machine import pick_family_name,pick_full_name_by_sex
+
+print('----------------cutting line-----------------------')
+for idx in range(10):
+    new_name=pick_a_full_name()
+print(new_name)
+print('----------------cutting line-----------------------')
+for idx in range(10):
+    new_name=nm.pick_a_full_name_by_sex(1)
+print(new_name)
